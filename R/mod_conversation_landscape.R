db76920f620495bea739d06c0d15ff55e011a56d
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-01-26 13:00:11 +0000

    move landing_page stuff to conversation_landscape module

--- R/mod_conversation_landscape.R
+++ R/mod_conversation_landscape.R
@@ -10,10 +10,51 @@
 mod_conversation_landscape_ui <- function(id){
   ns <- NS(id)
   tagList(
-    textInput("dummy_text",
-              label =  "Enter some text",
-              value = with_red_star("placeholder"))
+    gotop::use_gotop(),
+    shiny::fluidRow(
+      shiny::column(2,
+                    style = "padding-right: 0px; border: none;",
+                    shiny::textInput(inputId = ns("remainingName"), "All Data",
+                                     value = NULL,
+                                     placeholder = "filename")
+      ),
+      shiny::column(1,
+                    style = "padding-left: 10px; padding-right: 20px;",
+                    shiny::div(
+                      style = "margin-top: 25px;",
+                      shiny::downloadButton(ns("downloadAll"), "Download",
+                                            class = "btn btn-warning",
+                                            style = "background: #ff4e00; border-radius: 100px; color: #ffffff; border:none;")
+                    )),
+      shiny::column(3, style = "padding-left: 20px; padding-right: 10px;", shinyWidgets::searchInput(
+        #Use the shinyWidget searchInput for a tidy searchable button
+        inputId = ns("filterPattern"),
+        label = "Pattern to search text with",
+        placeholder = "A placeholder",
+        btnSearch = shiny::icon("search"),
+        btnReset = shiny::icon("remove"),
+        width = "100%",
+        value = ""
+      )),
+      shiny::column(2, shiny::textInput(ns("fileName"),
+                                        "Selected Data",
+                                        value = NULL,
+                                        placeholder = "filename excluding .csv")),
+      shiny::column(2, shiny::div(
+        style = "margin-top: 25px;",
+        shiny::downloadButton(ns("downloadData"),
+                              "Download",
+                              class = "btn btn-warning",
+                              style = "background: #ff4e00; border-radius: 100px; color: #ffffff; border:none;"
+        )
+      )),
+      mod_umap_plot_ui("umapPlot"),
+      mod_data_table_ui(ns("dataTable"))
+      # shiny::column(5,
+      #               shinycssloaders::withSpinner(
+      #                 DT::dataTableOutput(ns("highlightedTable"))))
 
+    )
   )
 }
 
@@ -21,8 +62,28 @@ mod_conversation_landscape_ui <- function(id){
 #'
 #' @noRd
 mod_conversation_landscape_server <- function(id){
-  moduleServer( id, function(input, output, session){
+  moduleServer(
+    id,
+    function(input, output, session){
     ns <- session$ns
+
+    mod_data_table_server("dataTable")
+    # output$highlightedTable <- DT::renderDataTable({
+    #   dt <- LandscapeR::ls_example
+    #
+    #   dt <- dt %>%
+    #     dplyr::select(date, text, cluster, sentiment, permalink) %>%
+    #     DT::datatable(
+    #       filter = "top",
+    #       options = list(pageLength = 25,
+    #                      dom = '<"top" ifp> rt<"bottom"lp>',
+    #                      autoWidth = FALSE),
+    #       style = "bootstrap",
+    #       rownames = FALSE,
+    #       escape = FALSE)
+    #
+    #   dt
+    # }) #This is working now after fixing the ns() stuff
 
   })
 }
--- R/mod_landing_page.R
+++ R/mod_landing_page.R
@@ -10,44 +10,7 @@
 mod_landing_page_ui <- function(id){
   ns <- NS(id)
   tagList(
-    gotop::use_gotop(),
-    shiny::fluidRow(
-      shiny::column(2,
-                    style = "padding-right: 0px; border: none;",
-                    shiny::textInput("remainingName", "All Data",
-                                     value = NULL,
-                                     placeholder = "filename")
-                    ),
-      shiny::column(1,
-                    style = "padding-left: 10px; padding-right: 20px;",
-                    shiny::div(
-                      style = "margin-top: 25px;",
-                      shiny::downloadButton("downloadAll", "Download",
-                                            class = "btn btn-warning",
-                                            style = "background: #ff4e00; border-radius: 100px; color: #ffffff; border:none;")
-                    )),
-      shiny::column(3, style = "padding-left: 20px; padding-right: 10px;", shinyWidgets::searchInput(
-        #Use the shinyWidget searchInput for a tidy searchable button
-        inputId = "filterPattern",
-        label = "Pattern to search text with",
-        placeholder = "A placeholder",
-        btnSearch = shiny::icon("search"),
-        btnReset = shiny::icon("remove"),
-        width = "100%",
-        value = ""
-      )),
-      shiny::column(2, shiny::textInput("fileName", "Selected Data", value = NULL, placeholder = "filename excluding .csv")),
-      shiny::column(2, shiny::div(
-        style = "margin-top: 25px;",
-        shiny::downloadButton("downloadData",
-                              "Download",
-                              class = "btn btn-warning",
-                              style = "background: #ff4e00; border-radius: 100px; color: #ffffff; border:none;"
-        )
-      )),
-      # mod_umap_plot("umapPlot")
-
-    )
+    p("Helpful UI Text")
   )
 }
 
