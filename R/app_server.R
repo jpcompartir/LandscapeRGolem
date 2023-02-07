1a3bc602497a6310a9194c3bf08e653fd44c5f29
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-02-07 16:38:17 +0000

    Start adding reactive logic to app_server, ensuring the UI is taken care of for the conversation_landscape module

--- R/app_server.R
+++ R/app_server.R
@@ -7,17 +7,24 @@
 app_server <- function(input, output, session) {
   # Your application server logic
 
+  #Create reactive data from data. Filters on inputs of sliders in umap_plot, defaulting values to 10.
+  #Then create a reactive dependency on remove_range$keep_keys, s.t. any change in remove_range makes a change here.
+  #Keys from selected_range move into remove_range$remove_keys when the delete button is pressed. $remove_keys and $keep_keys are the inverse of one another.
+  #reactive_data() also takes care of the filtering via a pattern
   reactive_data <- shiny::reactive({
 
-    dt <- LandscapeR::ls_example
+    data <- LandscapeR::ls_example
 
-    # dt %>%
-    #   dplyr::filter(V1> input$x1[[1]], V1 < input$x1[[2]], V2 > input$y1[[1]], V2 < input$y1[[2]]) %>%
-    #   dplyr::filter({{ id }} %in% remove_range$keep_keys) %>%
-    #   dplyr::filter(grepl(input$filterPattern, {{ text_var }}, ignore.case = TRUE))
-    dt
+    data <- data %>%
+      dplyr::filter(V1> input$x1[[1]], V1 < input$x1[[2]], V2 > input$y1[[1]], V2 < input$y1[[2]]) %>% #Slider input ranges
+      dplyr::filter({{ id }} %in% remove_range$keep_keys) %>% #Filtering for the keys not in remove_range$remove_keys
+      dplyr::filter(grepl(input$filterPattern, {{ text_var }}, ignore.case = TRUE))
+
+    return(data)
 
   })
+
+
 
   mod_conversation_landscape_server("landscapeTag", xd = reactive_data)
 }
