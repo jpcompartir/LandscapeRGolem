a2191dc21ae5781c80ac8f68bd7517ce65abd74f
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-02-14 15:55:54 +0000

    add dsitribution tab first logic

--- R/mod_distribution_tab.R
+++ R/mod_distribution_tab.R
@@ -10,17 +10,18 @@
 mod_distribution_tab_ui <- function(id){
   ns <- NS(id)
   tagList(
-
+    mod_volume_over_time_ui(id = ns("volumeModule"))
   )
 }
 
 #' distribution_tab Server Functions
 #'
 #' @noRd
-mod_distribution_tab_server <- function(id){
-  moduleServer( id, function(input, output, session){
+mod_distribution_tab_server <- function(id, highlighted_dataframe){
+  moduleServer(id, function(input, output, session){
     ns <- session$ns
 
+    mod_volume_over_time_server(id = "volumeModule", highlighted_dataframe = highlighted_dataframe)
   })
 }
 
