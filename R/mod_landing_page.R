2b331221d07783cbf254bf2aa8b6a03e20fd959a
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-03-01 16:13:54 +0000

    Build out dummy landing page using some css/html

--- R/mod_landing_page.R
+++ R/mod_landing_page.R
@@ -11,21 +11,33 @@ mod_landing_page_ui <- function(id){
   ns <- NS(id)
   tagList(
     shiny::fluidRow(
-      shiny::column(5,
+      shiny::column(6,
                     shiny::HTML("
-                  <h1>Welcome to <span style='color: #0f50d2;'>LandscapeR</span></h1>
+                  <h1>Welcome to <span style='color: #0f50d2;'>LandscapeR!</span></h1>
                   <br>
-                  <p><span style='color: #0f50d2;'>LandscapeR</span> is SHARE and Capture's application for creating navigable conversation maps from unstructured text data</p>
-                  <p>There are currently <b>four tabs</b> you can work with. The main tab is 'Survey Landscape', each of the remaining tabs require you to have selected data inside this tab, for their various plots to render.</p>
-                  <li><b>Bigram Network</b> is for rendering a bigram network from your highlighted data. This will help you to gauge how clean your selections are. </li>
-                  <li><b>Distribution Tab</b> is for looking at your selection's volume over time, sentiment distribution, and frequent words.</li>
-                  <li><b>Weighted Log-odds</b> is for comparing the distinctness of text between groups</li>
+                  <p><span style='color: #0f50d2;'>LandscapeR</span> is SHARE and Capture's application for creating navigable conversation maps from unstructured text data. This application will allow you to interactively clean, inspect and chart a conversation which has been embedded, clustered, and had its dimensions reduced.</p>
                   <br>
-                  <h2>Starting out</h2>
-                  <p>Head over to the Survey Landscape tab, use your mouse or trackpad to select regions of the conversation landscape, and watch the data table populate to the right. You can search for specific keywords using the pattern widget.</p>
-                  ")
-                    ),
+                  <p><b>If you are familiar</b> with  <span style='color: #0f50d2;'>LandscapeR</span>, you should <b>move directly</b> to the <b>Survey Landscape tab</b> to start analysing your mapped conversation.
 
+                  "),
+                    ),
+    ),
+    shiny::fluidRow(
+      shiny::column(3,
+                    shiny::HTML("<h2><b>Starting Out</b></h2>
+                  <p>Head over to the Survey Landscape tab, use your mouse or trackpad to select regions of the conversation landscape, and watch the data table populate to the right. You can search for specific keywords using the pattern widget.</p>
+                  ")),
+      shiny::column(3,
+                    shiny::HTML("<h2><b>Downloading Data</b></h2>
+                               <p>You can download all of the remaining data, or only the selected data using the download widgets. If you want to delete some data, you must highlight it first and then click the delete button found in the bottom right corner of the map. Your deletions will be reflected when you download all data.</p>")),
+      shiny::column(6,
+                    shiny::HTML("<h2><b>Viewing Summary Plots</b></h2>
+                                <p>There are currently <b>three plotting tabs</b> you can work with.</p>
+                      <li><b>Bigram Network</b> is for rendering a bigram network from your highlighted data. This will help you to gauge how clean your selections are. </li>
+                      <li><b>Distribution Tab</b> is for looking at your selection's volume over time, sentiment distribution, and frequent words.</li>
+                  <li><b>Weighted Log-odds</b> is for comparing the distinctness of text between groups</li>
+                  <br>")
+                    )
     )
   )
 }
