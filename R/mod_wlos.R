0b2e0de3b550c4c56148125ea9ecc84a137581ec
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-07-25 16:31:05 +0100

    add a nested-accordion version of mod_wlos and pass r to keep track of the global grouping variable

--- R/mod_wlos.R
+++ R/mod_wlos.R
@@ -12,69 +12,86 @@ mod_wlos_ui <- function(id) {
   tagList(
     bslib::page_fillable(
       bslib::accordion(
-        id = ns("accordion"),
+        id = ns("mainAccordion"),
         bslib::accordion_panel(
           id = ns("item1"),
-          title = "Weighted log-odds",
+          title = "Weighted Log-odds",
           active = TRUE,
-          wlos_text()
-        ),
-      ),
-      bslib::layout_sidebar(
-        fill = TRUE,
-        bslib::sidebar(
-          bslib::accordion(
-              id = ns("accordion2"),
-            bslib::accordion_panel(
-              id = ns("item2"),
-              title = "Aesthetic Controls",
-              active = FALSE,
-                shiny::sliderInput(inputId = ns("height"), "height", min = 100, max = 1400, value = 800, step = 100),
-                shiny::sliderInput(inputId = ns("width"), "width", min = 100, max = 1200, value = 600, step = 100),
-                shiny::sliderInput(inputId = ns("textSize"), "text size", min = 2, max = 8, value = 4, step = 1),
-                shiny::sliderInput(inputId = ns("nrow"), label = "number of rows", min = 1, max = 20, value = 10, step = 1),
-            ),
-            bslib::accordion_panel(
-              id = ns("item3"),
-              title = "Parameters",
-              active = TRUE,
-              shiny::sliderInput(
-                inputId = ns("topN"),
-                label = "top_n",
-                min = 15,
-                max = 60,
-                value = 30,
-                step = 5
-              ),
-              shiny::sliderInput(
-                inputId = ns("termCutoff"),
-                label = "top terms cutoff",
-                min = 500,
-                max = 5000, value = 2500,
-                step = 100
-              ),
-              shiny::selectInput(
-                inputId = ns("groupVar"),
-                label = "select your grouping variable",
-                choices = NULL
-              ),
-              shiny::downloadButton(outputId = ns("saveWLOs"), class = "btn btn-warning")
-            ),
-          ),
-        ),
-        bslib::accordion(
-          id = ns("accordion3"),
           bslib::accordion_panel(
-            id = ns("item3"),
-            title = "WLOs plot",
-            active = FALSE,
-            shinycssloaders::withSpinner(shiny::plotOutput(ns("wlosPlot"),
-                                                           height = "800px",
-                                                           width = "600px")
+            id = ns("wlosText"),
+            title = "Settings & Inputs",
+            active = TRUE,
+            wlos_text() #saved in zzz.R
+          ),
+          bslib::card(
+            full_screen = TRUE,
+            bslib::layout_sidebar(
+              fill = TRUE,
+              bslib::sidebar(bg = "white",
+                             bslib::accordion(
+                               id = ns("nestedAccordion1"),
+                               bslib::accordion_panel(
+                                 id = ns("nested1item1"),
+                                 title = "Aesthetic Controls",
+                                 open = FALSE,
+                                 icon = shiny::icon(
+                                   name = "wand-magic-sparkles"),
+                                 shiny::sliderInput(inputId = ns("height"), "height", min = 100, max = 1400, value = 800, step = 100),
+                                 shiny::sliderInput(inputId = ns("width"), "width", min = 100, max = 1200, value = 600, step = 100),
+                                 shiny::sliderInput(inputId = ns("textSize"), "text size", min = 2, max = 8, value = 4, step = 1),
+                                 shiny::sliderInput(inputId = ns("nrow"), label = "number of rows", min = 1, max = 20, value = 10, step = 1),
+                               ),
+                               bslib::accordion_panel(
+                                 id = ns("nested1item2"),
+                                 title = "Parameters",
+                                 open = TRUE,
+                                 icon = shiny::icon("wrench"),
+                                 shiny::sliderInput(
+                                   inputId = ns("topN"),
+                                   label = "top_n",
+                                   min = 15,
+                                   max = 60,
+                                   value = 30,
+                                   step = 5
+                                 ),
+                                 shiny::sliderInput(
+                                   inputId = ns("termCutoff"),
+                                   label = "top terms cutoff",
+                                   min = 30,
+                                   max = 5000, value = 2500,
+                                   step = 100
+                                 ),
+                                 # shiny::selectInput(
+                                 #   inputId = ns("groupVar"),
+                                 #   label = "select your grouping variable",
+                                 #   choices = NULL
+                                 # ),
+                                 shiny::downloadButton(outputId = ns("saveWLOs"), class = "btn btn-warning")
+                               ),
+                             ),
+              ),
+              bslib::accordion(
+                id = ns("nestedAccordion2"),
+                bslib::accordion_panel(
+                  id = ns("nested2item1"),
+                  title = "WLOs plot",
+                  active = FALSE,
+                  icon = shiny::icon("cloud-word"),
+                  # bslib::card( full_screen = TRUE,
+                  shinycssloaders::withSpinner(
+                    shiny::plotOutput(
+                      ns("wlosPlot"),
+                      height = "800px",
+                      width = "600px"
+                    )
+                  )
+                  # ) #blisb::card below icon =
+                )
+              )
             )
           )
-        )
-      )
+        ),
+      ),
     )
   )
 }
@@ -82,17 +99,18 @@ mod_wlos_ui <- function(id) {
 #' wlos Server Functions
 #'
 #' @noRd
-mod_wlos_server <- function(id, highlighted_dataframe) {
+mod_wlos_server <- function(id, highlighted_dataframe, r) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    observe({
-      shiny::updateSelectInput(session,
-                               inputId = "groupVar",
-                               choices = names(highlighted_dataframe()),
-                               selected = "cluster"
-      )
-    })
+    #Comment to move to global groupvariable
+    # observe({
+    #   shiny::updateSelectInput(session,
+    #                            inputId = "groupVar",
+    #                            choices = names(highlighted_dataframe()),
+    #                            selected = "cluster"
+    #   )
+    # })
 
     wlos_reactive <- reactive({
       if (nrow(highlighted_dataframe()) < 1) {
@@ -101,7 +119,7 @@ mod_wlos_server <- function(id, highlighted_dataframe) {
 
       wlos <- highlighted_dataframe() %>%
         LandscapeR::ls_wlos(
-          group_var = input$groupVar,
+          group_var = r$globalGroupVar,
           text_var = clean_text,
           top_n = input$topN,
           top_terms_cutoff = input$termCutoff,
