440e9a23c142ddd411d4d625b29f5c7db4d284aa
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-07-26 13:19:44 +0100

    Add button to control group and sub groups update

--- R/mod_c_groups_global.R
+++ R/mod_c_groups_global.R
@@ -11,37 +11,32 @@ mod_c_groups_global_ui <- function(id){
   ns <- NS(id)
   tagList(
     shiny::fluidRow(
+      id = ns("groupsRow"),
       shiny::column(
-        2,
+        3,
         shiny::selectInput(
           inputId = ns("groupVarGlobal"),
-          label = "select your grouping variable",
+          label = "grouping variable",
           choices = NULL)
       ),
-        shinyWidgets::pickerInput(
-          inputId = ns("subGroups"),
-          label = "Select:",
-          choices = NULL,
-          options = pickerOptions(
-            class = 'custom-picker',
-            actionsBox = TRUE,
-            size = 10,
-            selectedTextFormat = "count > 3"),
-          multiple = TRUE
-        )
+      shinyWidgets::pickerInput(
+        inputId = ns("subGroups"),
+        label = "levels of grouping variable",
+        choices = NULL,
+        options = shinyWidgets::pickerOptions(
+          class = 'custom-picker',
+          actionsBox = TRUE,
+          size = 10,
+          selectedTextFormat = "count > 3"),
+        multiple = TRUE
+      ),
+      shiny::column(1,
+                    shiny::actionButton(
+                      class = "btn-subgroups-update",
+                      inputId = ns("updateSubgroupsButton"),
+                      label = "Go")
+      )
     )
-
-    # shiny::selectizeInput(
-    #   inputId = ns("subGroups"),
-    #   label = "select your subgroups",
-    #   choices = NULL,
-    #   multiple = TRUE
-    # )
-    #   shiny::checkboxGroupInput(ns("subGroups"),
-    #                             label = "Select your subgroups",
-    #                             choices = NULL,
-    #                             inline = TRUE)
-
   )
 }
 
@@ -53,17 +48,26 @@ mod_c_groups_global_server <- function(id, highlighted_dataframe, r){
     ns <- session$ns
 
     observe({
-      shiny::updateSelectInput(session,
-                               inputId = "groupVarGlobal",
-                               choices = colnames(highlighted_dataframe()),
-                               selected = "cluster"
+      if (nrow(highlighted_dataframe()) > 1) {
+        shinyjs::enable("groupsRow")
+      } else {
+        shinyjs::disable("groupsRow")
+      }
+    })
+
+    observe({
+      shiny::updateSelectInput(
+        session,
+        inputId = "groupVarGlobal",
+        choices = colnames(highlighted_dataframe()),
+        selected = "cluster"
       )
     })
 
     observe({
       req(input$groupVarGlobal)
 
-      r$globalGroupVar <- input$groupVarGlobal
+      r$global_group_var <- input$groupVarGlobal
 
       my_choices <- unique(
         highlighted_dataframe()[[input$groupVarGlobal]]
@@ -75,8 +79,25 @@ mod_c_groups_global_server <- function(id, highlighted_dataframe, r){
         choices = my_choices,
         selected = my_choices
       )
+
+      #Initialise with the current selections (to render plot when tab opens)
+      r$current_subgroups <- my_choices
+      r$new_subgroups <- my_choices
     })
 
+    #Pass sub groups to r$
+    observeEvent(input$subGroups, {
+      r$global_subgroups <- input$subGroups
+    })
+
+    observe({
+      r$new_subgroups <- input$subGroups
+    })
+
+    observeEvent(input$updateSubgroupsButton, {
+      #Update the current subgroups when the button is pressed
+      r$current_subgroups <- r$new_subgroups
+    })
 
   })
 }
