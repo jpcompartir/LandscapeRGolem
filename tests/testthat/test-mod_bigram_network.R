31418a3d88ea7218d355e97e5a66be87eb2bcb20
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-11-06 10:41:00 +0000

    Add mod_bigram_network_server UI and server tests.

--- tests/testthat/test-mod_bigram_network.R
+++ tests/testthat/test-mod_bigram_network.R
@@ -1,92 +1,86 @@
 # #Informal test to see if highlighted_dataframe() can be turned into a plot, i.e. it can mimic the reactive behaviour needed elsewhere
 # generate_dummy_data() %>%
 #   make_bigram_viz(text_var = text, top_n = 5, min = 5, remove_stopwords = TRUE)
+test_that("mod_bigram_network_server generates a plot with correct inputs",{
+  testServer(
+    app = mod_bigram_network_server,
 
-testServer(
-  app = mod_bigram_network_server,
+    # Add here your module params
+    args = list(
+      highlighted_dataframe = generate_dummy_data
+    ),
 
-  # Add here your module params
-  args = list(
-    highlighted_dataframe = generate_dummy_data
-  ),
+    expr = {
+      ns <- session$ns
 
-  expr = {
-    ns <- session$ns
+      # #Set input -> check input is set
+      session$setInputs(topN = 20)
+      expect_true(input$topN == 20)
 
-    # #Set input -> check input is set
-    session$setInputs(topN = 20)
-    expect_true(input$topN == 20)
+      session$setInputs(minFreq = 5)
+      expect_equal(input$minFreq, 5)
 
-    session$setInputs(minFreq = 5)
-    expect_equal(input$minFreq, 5)
+      expect_error(is.reactive(object_that_doesnt_exist))
+      expect_false(is.reactive("object_that_doesn't exist"))
+      # print(highlighted_dataframe())
+      session$setInputs(removeStopwords = TRUE)
+      expect_true(input$removeStopwords)
 
-    # expect_error(bigram_reactive())
+      session$setInputs(width = 666)
+      session$setInputs(height = 666)
+      expect_equal(input$width, 666)
+      expect_equal(input$height, 666)
 
-    expect_error(is.reactive(object_that_doesnt_exist))
-    expect_false(is.reactive("object_that_doesn't exist"))
-    # print(highlighted_dataframe())
-    session$setInputs(removeStopwords = TRUE)
-    expect_true(input$removeStopwords)
+      # Can use browser to step into the server when it's running.
+      # browser()
 
-    session$setInputs(width = 666)
-    session$setInputs(height = 666)
-    expect_equal(input$width, 666)
-    expect_equal(input$height, 666)
-
-    # expect_snapshot(print(bigram_reactive()))
-
-    xd <- bigram_reactive()
-    print(names(xd))
-    print(length(xd))
-    print(class(xd))
-    expect_true(is.reactive(bigram_reactive))
-
-
-    # print(highlighted_dataframe() %>%
-    #         tibble())
-    # print(highlighted_dataframe() %>%
-    #   make_bigram_viz(text_var = text, top_n = input$topN, min = input$minFreq))
-
-    # output$bigramPlotter <- shiny::renderPlot({
-    #   highlighted_dataframe() %>%
-    #     make_bigram_viz(text_var = text, top_n = input$topN, min = input$minFreq)
-    # })
-    #
-    # print(str(output$bigramPlotter))
-    # print(output$bigramPlotter)
-
-
-    #
-    # # Establish input isn't set
-    # expect_true(is.null(session$topN))
-    #
-    # # Should error as inputs not set yet
-    # expect_error(print(bigram_reactive()))
-    #
-    # expect_true(is.reactive(bigram_reactive))
-
-    # print(output$bigramPlot)
-
-
-
-    # Golem's given NS functions ----
-    # expect_true(
-    #   inherits(ns, "function")
-    # )
-    # expect_true(
-    #   grepl(id, ns(""))
-    # )
-    # expect_true(
-    #   grepl("test", ns("test"))
-    # )
+      expect_true(is.reactive(bigram_reactive))
+      xd <- bigram_reactive()
+      expect_true(all(c('data', "layers", "scales", "mapping") %in% names(xd)))
+      expect_true(inherits(xd, "gg"))
+    })
 })
 
-# test_that("module ui works", {
-#   ui <- mod_bigram_network_ui(id = "test")
-#   golem::expect_shinytaglist(ui)
-#   # Check that formals have not been removed
-#   fmls <- formals(mod_bigram_network_ui)
-#   for (i in c("id")){
-#     expect_true(i %in% names(fmls))
-#   }
+
+test_that("module ui works", {
+  ui <- mod_bigram_network_ui(id = "test")
+  golem::expect_shinytaglist(ui)
+
+  # Check that formals have not been removed
+  fmls <- formals(mod_bigram_network_ui)
+  for (i in c("id")){
+    expect_true(i %in% names(fmls))
+  }
+  ui_string <-as.character(ui)
+  stringr::str_detect(ui_string, stringr::fixed('id="test-removeStopwords'))
+
+
+  # We don't find IDS that we shouldn't:
+  expect_false(stringr::str_detect(ui_string, stringr::fixed('id="test-bogiesN')))
+
+  #Ids to be applied all at once in the the vapply line of code below:
+  ids_that_should_exist <- list(
+    stringr::fixed('id="test-topN'),
+    stringr::fixed('id="test-bigramPlot"'),
+    stringr::fixed('id="test-minFreq"'),
+    stringr::fixed('id="test-removeStopwords"'),
+    stringr::fixed('id="test-updatePlotsButton"'),
+    stringr::fixed('id="test-height-label'),
+    stringr::fixed('id="test-updatePlotsButton')
+  )
+
+  #Using the *apply family rather than map as LandscapeRGolem has no {purrr} dep.
+  vapply(
+    ids_that_should_exist,
+    FUN = function(x) expect_true(stringr::str_detect(ui_string, pattern = x)),
+    FUN.VALUE =logical(length(1L))
+  )
+})
+
+
+# #Each time you run this snapshot you get different values for:
+# bslib-sidebar-2570, not sure why that is/would be but makes me think snapshots may not be the right way to go
+# test_that("module UI's snapshot persists", {
+#   set.seed(1234)
+#   expect_snapshot(mod_bigram_network_ui("test"))
 # })
