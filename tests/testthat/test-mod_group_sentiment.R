bf4951178468ecea0744dad2f4265a7dc253cd1f
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-11-07 16:38:30 +0000

    add group_sentiment server test for plot output, inputs + ui tests

---
+++ tests/testthat/test-mod_group_sentiment.R
@@ -0,0 +1,92 @@
+test_that("Module's server errors if given an incorrect input", {
+  expect_error(testServer(
+    mod_group_sentiment_server,
+    args = list(
+      bad_input = "really bad!")),
+    "unused argument"
+  )
+})
+
+test_that("Module's server function accepts the right named inputs", {
+  expect_silent(
+    testServer(
+      mod_group_sentiment_server,
+      # Add here your module params
+      args = list(
+        highlighted_dataframe = list(),
+        r = list()
+      ),
+      expr = {
+        ns <- session$ns
+      })
+  )
+})
+
+test_that("Module produces the plot as expected", {
+  testServer(
+    app = mod_group_sentiment_server,
+    args = list(
+      highlighted_dataframe = generate_dummy_data,
+      r = shiny::reactiveValues(global_group_var = "cluster",
+                                current_subgroups = 1:5)
+    ),
+    expr = {
+      ns <- session$ns
+
+      session$setInputs(
+        width = "450px",
+        height = "450px",
+        chartType = "volume",
+        labelsType = "volume")
+
+      expect_true(is.reactive(group_sent_reactive))
+      plot <- group_sent_reactive()
+
+      expect_true(inherits(plot, "gg"))
+      expect_true(all(c("group_var", "sentiment_var", "n", "percent", ".total", "percent_character") %in% names(plot$data)))
+    }
+  )
+})
+
+
+test_that("module ui works", {
+  ui <- mod_group_sentiment_ui(id = "test")
+  golem::expect_shinytaglist(ui)
+
+  ui_char <- as.character(ui)
+
+  #icons
+  c("wand-magic-sparkles", "wrench")
+
+  expect_true(stringr::str_detect(ui_char, "test-nestedAccordion1"))
+  expect_true(stringr::str_detect(ui_char, "test-height"))
+  expect_true(stringr::str_detect(ui_char, "test-width"))
+  expect_true(stringr::str_detect(ui_char, "test-labelsType"))
+  expect_true(stringr::str_detect(ui_char, "test-groupSentimentTitles"))
+  expect_true(stringr::str_detect(ui_char, "test-groupSentimentPlot"))
+  expect_true(stringr::str_detect(ui_char, "test-saveGroupSentiment"))
+
+  expect_true(
+    stringr::str_detect(
+      ui_char,
+      stringr::fixed('<div class="shiny-plot-output html-fill-item" id="test-groupSentimentPlot" style="width:450px;height:450px;"></div>')
+    )
+  )
+
+
+  expect_true(
+    stringr::str_detect(
+      ui_char,
+      stringr::fixed(
+        'aria-label="wrench icon"')
+    )
+  )
+
+  expect_true(
+    stringr::str_detect(
+      ui_char,
+      stringr::fixed(
+        '<i class="fas fa-wand-magic-sparkles" role="presentation" aria-label="wand-magic-sparkles icon"></i>')
+    )
+  )
+})
